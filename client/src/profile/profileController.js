9702e751d66463ac96eb2e2fef92a76a2236e2bd
Sebastian Valdivia <sebastianvaldivia@gmail.com>
2015-09-26 11:53:29 -0700

    Adding styling to profile page

--- client/src/profile/profileController.js
+++ client/src/profile/profileController.js
@@ -19,14 +19,38 @@ angular.module('spark-wild').controller( 'profileCtrl', [ '$rootScope', '$scope'
     }
   });
 
-  $scope.activities = [
-    'hiking',
-    'biking',
-    'walking',
-    'tennis',
-    'climbing',
-    'running'
-  ];
+  $scope.activities = {
+    hiking:{
+        key: "hiking",
+        label:"Hiking",
+        image:"/assets/hiking_button.png"
+    },
+    walking:{
+        key: "walking",
+        label:"Walking",
+        image:"/assets/walking_button.png"
+    },
+    biking:{
+        key: "biking",
+        label:"Biking",
+        image:"/assets/biking_button.png"
+    },
+    climbing:{
+        key: "climbing",
+        label:"Climbing",
+        image:"/assets/climbing_button.png"
+    },
+    tennis:{
+        key: "tennis",
+        label:"Tennis",
+        image:"/assets/tennis_button.png"
+    },
+    running:{
+        key: "running",
+        label:"Running",
+        image:"/assets/running_button.png"
+    }
+};
 
   $scope.selectedActivities = [];
   $scope.toggleSelection = function toggleSelection( activity ) {
