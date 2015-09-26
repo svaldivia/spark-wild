69cd07cdfea747c5f473545f930bff9aba7043fc
Joey Kudish <joachim.kudish@gmail.com>
2015-09-25 22:52:52 -0700

    app.js fixes + add Facebook Provider

--- client/src/app.js
+++ client/src/app.js
@@ -1,10 +1,13 @@
 'use strict';
 
-angular.module('spark-wild', ['ngResource', 'ngRoute', 'ngLodash','uiGmapgoogle-maps'] )
-	.config( ['uiGmapGoogleMapApiProvider', function(GoogleMapApiProvider) {
+angular.module('spark-wild', ['ngResource', 'ngRoute', 'ngLodash','uiGmapgoogle-maps', 'facebook'] )
+	.config( ['uiGmapGoogleMapApiProvider', 'FacebookProvider', function( GoogleMapApiProvider, FacebookProvider ) {
     	GoogleMapApiProvider.configure({
 	        key: 'AIzaSyAKU4eqwTJt46Wj3e7ybNKOdOcF9JekjTA',
 	        v: '3.20', //defaults to latest 3.X anyhow
 	        libraries: 'weather,geometry,visualization'
-    });
-}]);
+    	});
+		FacebookProvider.init( '1664935427126975' );
+	}]);
+
+var socket = io();
