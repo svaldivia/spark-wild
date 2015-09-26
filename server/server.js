ed591254b7ffef44543f0c9093202050062c62e4
Joey Kudish <joachim.kudish@gmail.com>
2015-09-25 17:53:19 -0700

    fix the url used in the console

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ require( './app/routes' )( app, config );
 app.listen( config.port );
 
 if ( 'development' === config.env ) {
-	console.log( "App's runnin'! visit http://spark.dev:" + config.port + ' to check it out.' );
+	console.log( "App's runnin'! visit http://spark-wild.dev:" + config.port + ' to check it out.' );
 }
 
 module.exports = app;
