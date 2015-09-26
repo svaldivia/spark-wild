05a44bc2117522c9c794c5fbda31c70346c2d622
Sarah Perez <karatezeus21@gmail.com>
2015-09-26 02:34:29 -0700

    Added click functionality on map marker

--- client/src/map/mapController.js
+++ client/src/map/mapController.js
@@ -7,20 +7,50 @@ angular.module('spark-wild').controller('mapCtrl', ['$scope','uiGmapGoogleMapApi
 			//zoom 1 = world, 20 is smallest zoom.
 			var startZoom = 15
 
-			//Load user location here
+			//to do: Load user location here
 			var userLat = 49.282089
 			var userLng = -123.107421
 
 	    	$scope.map = { center: { latitude: userLat, longitude: userLng }, zoom: startZoom };
 	    	$scope.options = {scrollwheel: false, disableDefaultUI: true}
 
+	    	//adding marker for user
 		    $scope.marker = {
 				id: 0,
 				coords: {
-					latitude: 49.282089,
-					longitude: -123.107421
-				}
+					latitude: userLat,
+					longitude: userLng
+				},
+		      options: { draggable: false },
+		      events: {
+		      	click: function(marker, eventName, args) {
+				$scope.map.zoom = 8;
+				},
+
+		        // dragend: function (marker, eventName, args) {
+		        //   $log.log('marker dragend');
+		        //   var lat = marker.getPosition().lat();
+		        //   var lon = marker.getPosition().lng();
+		        //   $log.log(lat);
+		        //   $log.log(lon);
+
+		        //   $scope.marker.options = {
+		        //     draggable: true,
+		        //     labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+		        //     labelAnchor: "100 0",
+		        //     labelClass: "marker-labels"
+		        //   };
+		        // }
+		      }
 		    };
+
+
+
+
+		    //To do: add markers for nearby activities/people
+		    //$scope.markers = [];
+		    
+		    console.log($scope.marker)
 
 
     });
