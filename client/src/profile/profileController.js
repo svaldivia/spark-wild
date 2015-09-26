e82265a9b9cd7f3e3783dae2b06c27eb47349eb9
Joey Kudish <joachim.kudish@gmail.com>
2015-09-26 01:12:47 -0700

    refactor/simplify the activity selection on profile page

--- client/src/profile/profileController.js
+++ client/src/profile/profileController.js
@@ -18,28 +18,23 @@ angular.module('spark-wild').controller( 'profileCtrl', [ '$rootScope', '$scope'
     }
   });
 
-  $scope.formData = {};
+  $scope.activities = [
+    'hiking',
+    'biking',
+    'walking',
+    'tennis',
+    'climbing',
+    'running'
+  ];
 
-  $scope.processForm = function(){
-    $location.path('/map');
-    $http({
-      method  : 'POST',
-      url     : '/saveuser',
-          data    : "some data",  // pass in data as strings
-          headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
-        })
-    .success(function(data) {
-      console.log(data);
-
-            // if (!data.success) {
-            //   // if not successful, bind errors to error variables
-            //   $scope.errorName = data.errors.name;
-            //   $scope.errorSuperhero = data.errors.superheroAlias;
-            // } else {
-            //   // if successful, bind success message to message
-            //   $scope.message = data.message;
-            // }
-    });
+  $scope.selectedActivities = [];
+  $scope.toggleSelection = function toggleSelection( activity ) {
+    var index = $scope.selectedActivities.indexOf( activity );
+    if ( index > -1) {
+      $scope.selectedActivities.splice( index, 1);
+    } else {
+      $scope.selectedActivities.push( activity );
+    }
   };
 
 }]);
