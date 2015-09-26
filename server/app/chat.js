358f753d57eb3aeee7427e4c53270e972ac83d9a
Joey Kudish <joachim.kudish@gmail.com>
2015-09-26 15:02:20 -0700

    chat hack

--- server/app/chat.js
+++ server/app/chat.js
@@ -2,7 +2,7 @@ module.exports = function( app, config, http, io ) {
 	io.on('connection', function(socket){
 		socket.on('chat message', function(message){
 			io.emit('chat message', message);
-			io.emit( 'chat message', 'hi there joey!' )
+			io.emit( 'chat message', 'wanna go for another one tomorrow?' )
 		});
 	});
 }
