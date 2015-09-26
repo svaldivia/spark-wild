d16deba4919d938eea6d2f855290de154808d5bd
Joey Kudish <joachim.kudish@gmail.com>
2015-09-26 11:31:56 -0700

    implement user insert api

--- server/app/routes.js
+++ server/app/routes.js
@@ -1,3 +1,5 @@
+var User = require( './models/user' );
+
 module.exports = function( app, config ) {
 	app.get( '/partials/*', function( req, res ) {
 		res.render( config.rootPath + 'client/src/' + req.params[ 0 ] );
@@ -7,7 +9,34 @@ module.exports = function( app, config ) {
 		res.render( 'index', {isDev: app.isDev(), liveReloadPort: config.liveReloadPort} );
 	} );
 
-	app.post('/saveuser', function(req, res){
-		res.send("POST BRO");
+	app.post('/api/user', function(req, res){
+		if ( ! req.body.id ) {
+			res.status( 400 ).json( { error: 'Missing Facebook user ID' } );
+			return;
+		}
+
+		var user = {
+			facebook_id: req.body.id
+		};
+
+		if ( req.body.email ) {
+			user.email = req.body.email;
+		}
+
+		if ( req.body.name ) {
+			user.name = req.body.name;
+		}
+
+		if ( req.body.location ) {
+			user.latest_location = req.body.location;
+		}
+
+		User.update(
+    		{ facebook_id: user.facebook_id },
+    		{
+    			$set: user,
+    		},
+    		{ upsert: true }
+		);
 	});
 };
