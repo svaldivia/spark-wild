955c1b3c1ea5c4401b9d53b8c886a5a4056d2702
Joey Kudish <joachim.kudish@gmail.com>
2015-09-25 20:53:09 -0700

    implement super rudimentary v1 of chat

--- client/src/app.js
+++ client/src/app.js
@@ -1,3 +1,4 @@
 'use strict';
 
 angular.module('spark-wild', ['ngResource', 'ngRoute', 'ngLodash'] );
+var socket = io();
--- client/src/chat/chatController.js
+++ client/src/chat/chatController.js
@@ -1,4 +1,16 @@
 'use strict';
 
 angular.module('spark-wild').controller('chatCtrl', ['$scope', function($scope) {
+	$scope.messages = [];
+
+	$scope.submitMessage = function(){
+		socket.emit('chat message', this.newMessage);
+		$scope.newMessage = '';
+	};
+
+	socket.on('chat message', function( message ){
+		console.log( message );
+    	$scope.messages.push( message );
+    	$scope.$apply();
+  	});
 }]);
