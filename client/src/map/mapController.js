7283ad609ea6b899d2e51425ea1e2b4ed7e7db9c
Sarah Perez <karatezeus21@gmail.com>
2015-09-26 00:58:57 -0700

    map view centers on user and adds marker

--- client/src/map/mapController.js
+++ client/src/map/mapController.js
@@ -1,7 +1,27 @@
 'use strict';
 
 angular.module('spark-wild').controller('mapCtrl', ['$scope','uiGmapGoogleMapApi', function($scope, uiGmapGoogleMapApi) {
+	    
 	    uiGmapGoogleMapApi.then(function(maps) {
-	    	$scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+
+			//zoom 1 = world, 20 is smallest zoom.
+			var startZoom = 15
+
+			//Load user location here
+			var userLat = 49.282089
+			var userLng = -123.107421
+
+	    	$scope.map = { center: { latitude: userLat, longitude: userLng }, zoom: startZoom };
+	    	$scope.options = {scrollwheel: false, disableDefaultUI: true}
+
+		    $scope.marker = {
+				id: 0,
+				coords: {
+					latitude: 49.282089,
+					longitude: -123.107421
+				}
+		    };
+
+
     });
 }]);
