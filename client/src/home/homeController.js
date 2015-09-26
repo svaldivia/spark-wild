0c1affb1b50f4d0c19b4a7f57427fd7c42778438
Joey Kudish <joachim.kudish@gmail.com>
2015-09-25 23:48:11 -0700

    implement first version of FB login

--- client/src/home/homeController.js
+++ client/src/home/homeController.js
@@ -1,4 +1,19 @@
 'use strict';
 
-angular.module('spark-wild').controller('homeCtrl', ['$scope', function($scope) {
+angular.module('spark-wild').controller('homeCtrl', ['$rootScope', '$location', 'Facebook', '$scope', function($rootScope, $location, Facebook, $scope) {
+	$scope.error = false;
+
+	$scope.login = function() {
+		Facebook.login(function(response) {
+			if( response.status === 'connected' ) {
+				$rootScope.loggedIn = true;
+				$location.path( '/profile' );
+				$scope.error = false;
+			} else {
+				$rootScope.loggedIn = false;
+				$scope.error = "Please authorize the Facebook app";
+			}
+      	});
+	};
+
 }]);
